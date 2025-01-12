7c6bc37177f5fa48061d217087c013241446b9b5
hasudaaaaa <cazman@outlook.jp>
2025-01-12 23:29:04 +0900

    各ページのタイトルとOGP設定を更新し、リンクのパスを修正

--- js/humburber-menu.js
+++ js/humburber-menu.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     overlay.className = 'overlay';
     document.body.appendChild(overlay);
 
-    const menuOpenIcon = "images/icons/menu_24dp_5F6368_FILL0_wght300_GRAD0_opsz24.svg"; // 通常時
-    const menuCloseIcon = "images/icons/close_24dp_5F6368_FILL0_wght300_GRAD0_opsz24.svg"; // 展開時
+    const menuOpenIcon = "/images/icons/menu_24dp_5F6368_FILL0_wght300_GRAD0_opsz24.svg"; // 通常時
+    const menuCloseIcon = "/images/icons/close_24dp_5F6368_FILL0_wght300_GRAD0_opsz24.svg"; // 展開時
 
     menuButton.addEventListener('click', () => {
         const isActive = menuOverlay.classList.toggle('active');
