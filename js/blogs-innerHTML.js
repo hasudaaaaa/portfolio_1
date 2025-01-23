88af623bcd3594eabdbb19778211f5c48bf18146
hasudaaaaa <cazman@outlook.jp>
2025-01-24 01:51:45 +0900

    ブログページのデータ構造を更新し、HTMLおよびCSSを修正。

--- js/blogs-innerHTML.js
+++ js/blogs-innerHTML.js
@@ -1,19 +1,20 @@
 // 作品データを外部ファイルから読み込む
 fetch('/blogs/blogs.json')
     .then(response => response.json()) // JSON形式で読み込む
-    .then(artworks => {
+    .then(blogs => {
         let galleryContainer = document.getElementById('gallery-container');
-        artworks.forEach(artwork => {
+        blogs.forEach(blog => {
             // 埋め込むHTMLを作成
-            let artworkHTML = `
-                <li class="artworks-item">
-                    <a href="${artwork.pageURL}">
-                        <div class="thumbnail-frame"><img src="${artwork.thumbnailPath}"/></div>
-                        <h3 class="artworks-item-title">${artwork.title}</h3>
+            let blogHTML = `
+                <li class="blogs-item">
+                    <a href="${blog.pageURL}">
+                        <div class="thumbnail-frame"><img src="${blog.thumbnailPath}"/></div>
+                        <h3 class="blogs-item-title">${blog.title}</h3>
+                        <div class="blog-date"><small>${blog.date}</small></div>
                     </a>
                 </li>
             `;
-            galleryContainer.innerHTML += artworkHTML;
+            galleryContainer.innerHTML += blogHTML;
         });
     })
     .catch(error => console.error('データの読み込みに失敗しました:', error));
